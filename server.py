3e6b49d2677a431a1203bb798763cef806ececbb
Joe Schlessinger <joe.c.schlessinger@gmail.com>
2022-06-12 15:08:17 -0600

    minimum viable product for webserver

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for, request, send_from_directory
+
+from armymarkdown import memo_model, writer
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/process", methods=["GET", "POST"])
+def process():
+    if request.method == "POST":
+        text = request.form["raw_data"]
+
+    m = memo_model.parse_lines(text.split("\n"))
+    mw = writer.MemoWriter(m)
+    mw.write()
+    mw.generate_memo()
+
+    return send_from_directory(
+        "/home/joe/Documents/Programming/armymarkdown/",
+        "temp_file.pdf",
+        as_attachment=True,
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
