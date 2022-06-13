3b7fec96344e686a82364cada0af587d68851d27
Joe Schlessinger <joe.c.schlessinger@gmail.com>
2022-06-12 18:55:12 -0600

    fixed bug for including null author title

--- armymarkdown/memo_model.py
+++ armymarkdown/memo_model.py
@@ -55,8 +55,7 @@ def parse_lines(file_lines):
     try:
         memo_begin_loc = [i for i, s in enumerate(file_lines) if "SUBJECT" in s][0]
     except IndexError:
-        return """ERROR: missing the keyword SUBJECT.
-    Please add SUBJECT=(your subject) above the start of your memo"""
+        return """ERROR: missing the keyword SUBJECT. Please add SUBJECT=(your subject) above the start of your memo"""
 
     memo_begin_loc += 1
     for line in file_lines[:memo_begin_loc]:
