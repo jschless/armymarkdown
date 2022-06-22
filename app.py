ee2c8dd3285183d97b92fb9d8906d051a90c19e0
Joe Schlessinger <joe.c.schlessinger@gmail.com>
2022-06-21 20:24:56 -0600

    specify region aws

--- app.py
+++ app.py
@@ -35,7 +35,9 @@ s3 = boto3.client(
     "s3",
     aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-    config=boto3.session.Config(signature_version="s3v4"),
+    config=boto3.session.Config(
+        region_name="us-east-2", signature_version="s3v4"
+    ),
 )
 
 boilerplate_text = open("./memo_template.Amd", "r").read()
