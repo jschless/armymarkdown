81cb8516305f847f3900db22bdcbc9e319643404
jschless <joe.c.schlessinger@gmail.com>
2022-11-26 18:44:22 -0700

    Update writer.py

--- armymarkdown/writer.py
+++ armymarkdown/writer.py
@@ -21,7 +21,6 @@ class MemoWriter:
             self.output_file = self.temp_file
         with open(self.output_file, "w+") as f:
             print("\n".join(self.lines), file=f)
-            print("\n".join(self.lines))
 
     def generate_memo(self):
         subprocess.run(
