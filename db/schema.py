9625a54f588fb6d079b35ff933116f0bd5d95281
jschless <joe.c.schlessinger@gmail.com>
2024-04-11 17:42:11 -0400

    adding basic database support

---
+++ db/schema.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    documents = db.relationship("Document", backref="user", lazy=True)
+
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(1000))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
