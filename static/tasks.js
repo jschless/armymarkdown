c34949db698070cb3163f08bba73551a7526c877
jschless <joe.c.schlessinger@gmail.com>
2024-04-14 18:13:16 -0400

    check for popup, if not furnish button

--- static/tasks.js
+++ static/tasks.js
@@ -2,11 +2,36 @@ function update_progress(status_url, count) {
     // send GET request to status URL
     $.get(status_url, function (data) {
         if (data["state"] == "SUCCESS") {
-            $("#status").text("");
-	    document.getElementById('progress').style.width = '100%';
-	    document.getElementById('progress-bar-container').style.display = 'none';
-	    document.getElementById('progress').style.width = '0%';
-	    window.open(data["presigned_url"], "_blank"); // support multiple files
+
+	    $("#status").text("");
+	    windowOpened = window.open(data["presigned_url"], "_blank");
+	    if (windowOpened == null ) {
+		// Create a button to retrieve memo on user click
+		var button = document.createElement('button');
+		button.textContent = 'Click to open memo';
+		button.addEventListener('click', function() {
+		    window.open(data["presigned_url"], "_blank"); // support multiple files
+		    document.getElementById('progress-bar').style.width = '100%';
+		    document.getElementById('progress-bar-container').style.display = 'none';
+		    document.getElementById('progress-bar-container').style.opacity = '.5';
+		    document.getElementById('progress').style.width = '0%';
+		    document.getElementById("temp_button").remove();
+		});
+		// button.classList.add("u-full-width");
+		button.style.margin = '20px';
+		button.classList.add("center");
+		button.setAttribute('id', "temp_button");
+
+		var container = document.getElementById('progress-bar-container');		
+		container.style.opacity ='1';
+		container.append(document.createElement('br'));
+		container.appendChild(button);	    	
+	    } else {
+		document.getElementById('progress-bar-container').style.display = 'none';
+		document.getElementById('progress-bar').style.width = '100%';
+		document.getElementById('progress').style.width = '0%';
+		
+	    }	    
             return;
         } else if (data["state"] == "FAILURE") {
             $("#status").text(
@@ -20,10 +45,9 @@ function update_progress(status_url, count) {
 	    const averageSeconds = 10;
             // rerun in 1 seconds
             if (count < 80) {
-		// $("#status").text(
-		//     "Waiting for your memo pdf to be generated! Please be patient! It's only been " +
-		// 	count * 2 +
-		// 	" seconds."
+		// newWindow.document.write(
+		//      "Waiting for your memo pdf to be generated! Please be patient! It's only been " +
+		//  	count +	" seconds."
 		// );
 		let progress = Math.min(count / averageSeconds * 100, 100);
 		document.getElementById('progress').style.width = progress + '%';
