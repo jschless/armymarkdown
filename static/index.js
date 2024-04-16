50f01501c9f66ffd72f0862bf9a0e2c5a5a8cdbd
jschless <joe.c.schlessinger@gmail.com>
2024-04-16 19:26:59 -0400

    fixed saving

--- static/index.js
+++ static/index.js
@@ -1,17 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var currentUrl = window.location.pathname;
-    // Loop through the options and find the one that matches the current URL
-    var linkSelector = document.getElementById('linkSelector');
-    for (var i = 0; i < linkSelector.options.length; i++) {       
-	var option = linkSelector.options[i];
-	if (option.value === currentUrl) {
-            // Set the text of the option to the current URL
-            option.selected = true;
-            break;
-	}
-    }
-});
-
 makeTabsWork("editor");
 
 $(function () {
